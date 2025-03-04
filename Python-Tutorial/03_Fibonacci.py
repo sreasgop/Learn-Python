bcc55cded01b8f0eea73b16eaf00ee65adb318e2
sreasgop <sreasgop@gmail.com>
2025-03-04 08:15:20 +0530

    03_Fibonacci.py modified: Program re-written to print Fibonacci Series till nth term

--- Python-Tutorial/03_Fibonacci.py
+++ Python-Tutorial/03_Fibonacci.py
@@ -1,8 +1,22 @@
 # Fibonacci Number: 
 
+# n = int(input("Enter nth term: "))
+# a, b = 0, 1
+# while a < n:
+#     print(a, end=' ')
+#     a, b = b, a+b
+# print()
+
+
+# In this program we are generating the fibonacci series till the term is less than the entered value n. However if we want to create the fibonacci series till nth term we are supposed to do it in this manner. 
+
+# Genrating Fibonacci Series till nth term: 
 n = int(input("Enter nth term: "))
-a, b = 0, 1
-while a < n:
-    print(a, end=' ')
+a, b = 0, 1     # Setting the value of first two terms using multiple assignment of Python
+while n:
+    print(a, end=" ")
     a, b = b, a+b
+    n -= 1
 print()
+
+
